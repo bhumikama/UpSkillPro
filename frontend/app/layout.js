9de95e44fb83ab0bf105a0d163f119491c3093da
Hanna Vorontsova <159482282+antuanettaV@users.noreply.github.com>
2025-01-13 22:06:01 +0200

    updated files

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -1,5 +1,4 @@
 import "./globals.css";
-import Navbar from '../components/navbar/Navbar.jsx';
 
 export const metadata = {
   title: "HackYourFuture"
@@ -8,11 +7,8 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="flex flex-col min-h-screen">
-        <Navbar />
-        <main className="flex-grow flex justify-center items-center">
-          {children}
-        </main>
+      <body>
+        {children}
       </body>
     </html>
   );
