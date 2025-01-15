0418bff1e9260f45bf1e4719a54df33c2a769107
Hanna Vorontsova <159482282+antuanettaV@users.noreply.github.com>
2025-01-15 21:18:55 +0200

    adding the contact us form

--- frontend/app/contact-us/page.jsx
+++ frontend/app/contact-us/page.jsx
@@ -1,51 +1,67 @@
-import React from 'react';
-import Navbar from '@/components/Navbar/Navbar';
+"use client";
 
-const ContactForm = () => {
+import React, { useState } from "react";
+import Navbar from "@/components/Navbar/Navbar";
+
+const ContactUs = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   return (
-    <div className="flex flex-col justify-center items-center min-h-screen bg-gray-100">
-      <div className="bg-white rounded-xl shadow-md p-10 max-w-md w-full mt-10">
-        <form>
-          <div className="mb-5">
-            <input
-              type="text"
-              placeholder="Full Name"
-              className="w-full px-5 py-4 border-2 border-gray-300 rounded-full text-gray-600 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-black hover:border-white transition duration-300"
-            />
-          </div>
-          <div className="mb-5">
-            <input
-              type="email"
-              placeholder="Email"
-              className="w-full px-5 py-4 border-2 border-gray-300 rounded-full text-gray-600 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-black hover:border-white transition duration-300"
-            />
-          </div>
-          
-          <div className="mb-5">
-            <input
-              type="tel"
-              placeholder="Phone Number"
-              className="w-full px-5 py-4 border-2 border-gray-300 rounded-full text-gray-600 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-black hover:border-white transition duration-300"
-            />
-          </div>
-          
-          <div className="mb-5">
-            <textarea
-              rows="4"
-              placeholder="Your Message"
-              className="w-full px-5 py-4 border-2 border-gray-300 rounded-lg text-gray-600 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-black hover:border-white transition duration-300"
-            ></textarea>
-          </div>
-          <button
-            type="submit"
-            className="w-full py-4 bg-black text-white font-semibold rounded-full hover:bg-gray-800 transition duration-300"
-          >
-            SEND MESSAGE
-          </button>
-        </form>
+    <>
+      <Navbar isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
+
+      <div
+        className={`min-h-screen bg-gray-100 flex justify-center items-center pt-[80px] transition-all duration-300 ${
+          isMenuOpen ? "mt-[250px]" : "mt-[80px]"
+        }`}
+      >
+        <div className="bg-white shadow-lg rounded-xl p-8 sm:p-10 max-w-sm sm:max-w-lg w-full border-2 border-transparent hover:border-gray-300 transition-all duration-300">
+          <h2 className="text-center text-3xl sm:text-4xl font-semibold text-gray-800 mb-6 sm:mb-8 tracking-wide leading-snug">
+            Fill the form below to reach out to us
+          </h2>
+          <form className="space-y-4 sm:space-y-6">
+            <div>
+              <input
+                type="text"
+                placeholder="Full Name"
+                className="w-full p-3 sm:p-4 border border-black rounded-full text-gray-600 focus:outline-none focus:ring-2 focus:ring-gray-400"
+              />
+            </div>
+
+            <div>
+              <input
+                type="email"
+                placeholder="Email"
+                className="w-full p-3 sm:p-4 border border-black rounded-full text-gray-600 focus:outline-none focus:ring-2 focus:ring-gray-400"
+              />
+            </div>
+
+            <div>
+              <input
+                type="tel"
+                placeholder="Phone Number"
+                className="w-full p-3 sm:p-4 border border-black rounded-full text-gray-600 focus:outline-none focus:ring-2 focus:ring-gray-400"
+              />
+            </div>
+
+            <div>
+              <textarea
+                className="w-full p-3 sm:p-4 border border-black rounded-xl text-gray-600 focus:outline-none focus:ring-2 focus:ring-gray-400"
+                rows="4"
+              />
+            </div>
+
+            <button
+              type="submit"
+              className="w-full py-3 sm:py-4 font-semibold rounded-full bg-black text-white hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-gray-400"
+            >
+              SEND MESSAGE
+            </button>
+          </form>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
-export default ContactForm;
+export default ContactUs;
---
+++ frontend/components/HomePage/HomePage.jsx
@@ -0,0 +1,17 @@
+import HYFLogo from "@/assets/hyf.svg";
+import Image from "next/image";
+import "./HomePage.css";
+import Navbar from "../Navbar/Navbar";
+
+function HomePage() {
+  return (
+    <>
+        <Navbar /> 
+
+       
+         </>
+  );
+}
+
+export default HomePage;
+
