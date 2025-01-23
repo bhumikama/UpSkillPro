7e9f923caa5bd83f6eb92d7962dd62ff698565d9
RajeshBhatt <rjshbhatt9@gmail.com>
2025-01-23 13:49:37 -0800

    Created ClientLogo Component and corrected image path

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,10 +1,12 @@
 import HeroSection from "./_components/HomePageComponents/Hero";
 import CategorySection from "./_components/HomePageComponents/CategorySection";
+import ClientLogo from "./_components/HomePageComponents/ClientLogo";
 export default function Home() {
   return (
     <div>
       <HeroSection />
       <CategorySection />
+      <ClientLogo />
     </div>
   );
 }
