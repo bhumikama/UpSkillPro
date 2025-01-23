cda09ecef5413b4da444b65a4234ff37f32e14b2
Hanna Vorontsova <159482282+antuanettaV@users.noreply.github.com>
2025-01-23 20:13:17 +0200

    adding about us page

--- frontend/app/about-us/page.jsx
+++ frontend/app/about-us/page.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { FaLinkedin } from "react-icons/fa";
 import Image from "next/image";
 import Link from "next/link";
-import OurTeam from "@/components/OurTeam";
+import OurTeam from "../_components/HomePageComponents/OurTeam.jsx";
 
 const AboutUs = () => {
   return (
@@ -80,7 +80,7 @@ const AboutUs = () => {
 <OurTeam/>
         
       </main>
-    </>
+       </>
   );
 };
 
