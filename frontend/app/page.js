de95eb44d32f036ea4a4be8181bf31fe5a6e9f06
RajeshBhatt <rjshbhatt9@gmail.com>
2025-01-26 15:22:11 -0800

    no changes

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,11 +1,13 @@
 import HeroSection from "./_components/HomePageComponents/Hero";
 import CategorySection from "./_components/HomePageComponents/CategorySection";
+import TabSelector from "./_components/HomePageComponents/TabSelector";
 import ClientLogo from "./_components/HomePageComponents/ClientLogo";
 export default function Home() {
   return (
     <div>
       <HeroSection />
       <CategorySection />
+      <TabSelector />
       <ClientLogo />
     </div>
   );
