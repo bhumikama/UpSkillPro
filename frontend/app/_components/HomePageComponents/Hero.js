8bb7dcdb2cd746fe3885a851599b95d266356c9c
Oyelaja Hussein <premiumitng@gmail.com>
2025-01-27 18:39:33 +0100

    Made some changes to the hero component

--- frontend/app/_components/HomePageComponents/Hero.js
+++ frontend/app/_components/HomePageComponents/Hero.js
@@ -2,15 +2,15 @@ import Image from "next/image";
 
 export default function HeroSection() {
   return (
-    <section className="relative bg-black text-white ">
-      <div className=" mx-auto flex flex-col md:flex-row justify-center items-center gap-20 ">
-        <div className="text-center md:text-left  h-auto ">
-          <h1 className="text-4xl md:text-6xl font-bold md:leading-normal mb-4 ">
-            Learn, Grow, Slay. <br /> Your Future Starts Here.
+    <section className="relative bg-black text-white h-auto ">
+      <div className="container mx-auto flex flex-col md:flex-row justify-between items-center px-4 md:px-0">
+        <div className="text-center md:text-left  h-auto">
+          <h1 className="text-4xl md:text-7xl font-bold md:leading-normal mb-4 ">
+            Learn, Grow, Slay.  Your Future Starts Here.
           </h1>
           <p className="text-gray-400 text-sm md:text-base md:leading-snug leading-tight mb-6">
             Discover bite-sized courses, interactive tools, and a learning
-            experience <br /> designed to match your rhythm and ambition.
+            experience designed to match your rhythm and ambition.
           </p>
           <button className="bg-white text-black px-6 py-3 text rounded-full font-medium hover:bg-gray-200  transition">
             Explore Courses
@@ -19,8 +19,14 @@ export default function HeroSection() {
             Become An Instructor
           </button>
         </div>
-        <div className="relative ">
-          <Image src="/hero-image.png" alt="Hero" width="500" height="500" />
+        <div className="h-auto ">
+          <Image
+            src="/hero-image.png"
+            alt="Hero"
+            width="900"
+            height="600"
+            
+          />
         </div>
       </div>
     </section>
