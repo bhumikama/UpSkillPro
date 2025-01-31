92c8802651a1fd99f5923f64436ac7d9a96e02b2
Oyelaja Hussein <premiumitng@gmail.com>
2025-01-31 11:13:20 +0100

    Added icons to course price and instructor

--- frontend/app/_components/HomePageComponents/CourseCard.jsx
+++ frontend/app/_components/HomePageComponents/CourseCard.jsx
@@ -4,6 +4,9 @@ import { Badge } from "@/components/ui/badge";
 import { Card, CardContent } from "@/components/ui/card";
 import React from "react";
 import Link from "next/link";
+import { Currency, DollarSign, DollarSignIcon } from "lucide-react";
+import { Money } from "@mui/icons-material";
+import { FaChalkboardTeacher } from "react-icons/fa";
 
 const CourseCard = ({ course }) => {
   return (
@@ -25,11 +28,13 @@ const CourseCard = ({ course }) => {
           </p>
           <div className="flex items-center justify-between">
             <div className="flex items-center gap-1">
-              <h1 className="font-medium text-sm">Instructor:{" "}{course.instructor.name}</h1>
+              <h1 className="font-medium text-sm flex items-center gap-2">
+               <FaChalkboardTeacher className="text-gray-600"/> Instructor: {course.instructor.name}
+              </h1>
             </div>
           </div>
           <div className="text-lg font-bold flex justify-between items-center">
-            <span>Dkk {course.price}</span>
+            <span className="text-green-700 flex items-center"> <DollarSign size={20} className="text-gray-500"/> {course.price}{" "}Dkk</span>
             <Badge
               className={
                 "bg-blue-600 text-white px-2 py-1 text-xs rounded-full"
@@ -39,7 +44,6 @@ const CourseCard = ({ course }) => {
             </Badge>
           </div>
         </CardContent>
-        
       </Card>
     </Link>
   );
