4826537be763f00e130dc59eee26eac745f9d097
Oyelaja Hussein <premiumitng@gmail.com>
2025-02-01 15:35:51 +0100

    Added the authenticated username to the my learning page

--- frontend/app/my-learning/page.js
+++ frontend/app/my-learning/page.js
@@ -3,6 +3,7 @@ import React from "react";
 import { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import { useRouter } from "next/navigation";
+import Link from "next/link";
 import ProgressCard from "../student-dashboard/components/ProgressCard";
 
 const MyLearningPage = () => {
@@ -10,7 +11,7 @@ const MyLearningPage = () => {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
   const router = useRouter();
-
+  const user = useSelector((state) => state.auth.user);
   const { isAuthenticated } = useSelector((state) => state.auth);
 
   useEffect(() => {
@@ -58,18 +59,23 @@ const MyLearningPage = () => {
   }
 
   return (
-    <div className="max-w-4xl mx-auto my-10 px-4 md:px-0">
+    <div className="container mx-auto my-10 px-4 md:px-0">
+      <h3 className="text-2xl font-bold mb-3">
+        Welcome back! <span className="text-green-600">{user.name} 🎉</span>
+      </h3>
       <h1 className="font-bold text-2xl">MY LEARNING</h1>
       <div className="my-5">
         {enrolledCourses.length === 0 ? (
           <p>You are not enrolled in any course.</p>
         ) : (
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4">
             {enrolledCourses.map((course, index) => (
               <ProgressCard key={index} course={course} />
             ))}
           </div>
+          
         )}
+        
       </div>
     </div>
   );
