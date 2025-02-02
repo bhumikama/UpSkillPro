775c704f20d81aae9730c43597593fc9555915a6
Bhumika Mallikarjun <bhumikamallikarjun98@gmail.com>
2025-02-03 00:47:07 +0100

    added the missing images

--- frontend/store/StoreProvider.js
+++ frontend/store/StoreProvider.js
@@ -1,18 +1,3 @@
-// "use client";
-// import { store, persistor } from "./store";
-// import { Provider } from "react-redux";
-// import { PersistGate } from "redux-persist/integration/react";
-
-// export default function StoreProvider({ children }) {
-//   return (
-//     <Provider store={store}>
-//       <PersistGate loading={<p>Loading..</p>} persistor={persistor}>
-//         {children}
-//       </PersistGate>
-//     </Provider>
-//   );
-// }
-
 "use client";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
